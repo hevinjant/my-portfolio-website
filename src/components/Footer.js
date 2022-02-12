07c1952124dd0bd9ed9038dfa0ebf31eb04eff04
Hevin Jant <hevin.jant@gmail.com>
2022-02-12 04:32:06 -0800

    adjusted Projects' gap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ function Footer() {
         <EmailIcon /> hjantasm@calpoly.edu
       </div>
       <p>&copy; 2021 Hevin Jantasmin</p>
-      <p>ver.1.8</p>
+      <p>ver.1.9</p>
     </div>
   );
 }
