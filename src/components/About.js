bb990674997b9afe63be6e2c73fdaf9c8b45d8b6
Hevin Jant <hevin.jant@gmail.com>
2022-02-13 15:59:42 -0800

    added download link to resume

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { aboutMe } from "../Data";
+import Resume from "../assets/Hevin_Jantasmin_RESUME_Feb2022.pdf";
 
 function About() {
   return (
@@ -8,6 +9,11 @@ function About() {
       <div className="about" id="section-about">
         <h1>About Me</h1>
         <p>{aboutMe}</p>
+        <div className="download-link">
+          <a href={Resume} download>
+            Download my resume here
+          </a>
+        </div>
       </div>
     </>
   );
