ddaf06bd4c38279c4cf886c9fa10771d1cad546c
unknown <hevin.jant@gmail.com>
2022-10-08 08:44:19 -0700

    updated resume to sept2022

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { aboutMe } from "../Data";
-import Resume from "../assets/Hevin_Jantasmin_RESUME_Apr2022.pdf";
+import Resume from "../assets/HevinJantasmin_Resume_Sept2022.pdf";
 
 function About() {
   return (
