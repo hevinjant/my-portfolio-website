635d2804ba8909679cd66e01b7940d3a22ba771d
unknown <hevin.jant@gmail.com>
2023-02-20 18:32:54 -0800

    updated to react 18, updated MUI, updated background img

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,12 @@
 import React from "react";
-import Typed from "react-typed";
 import MyPic from "../assets/myPic.jpg";
-import Background1 from "../assets/bg1.jpg";
+import Background1 from "../assets/pbg3.jpg";
 
 function Home() {
   return (
     <div className="home" style={{ backgroundImage: `url(${Background1})` }}>
       <div className="left-home">
-        <Typed
-          className="typed-text"
-          strings={["Hi There! I'm Hevin.", "Welcome to my portfolio website."]}
-          typeSpeed={80}
-          backSpeed={30}
-          loop
-        />
+        <h1>Hi, I'm Hevin!</h1>
       </div>
       <div className="right-home">
         <img className="mypic" src={MyPic} />
